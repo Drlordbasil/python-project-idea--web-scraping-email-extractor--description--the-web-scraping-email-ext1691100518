6ca3a2998a51b4c36b341bf534d148904f07bd0b
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 21:09:11 -0400

    AI-enhanced and auto-formatted code

--- main.py
+++ main.py
@@ -1,103 +1,54 @@
-import re
-import requests
-import json
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
-from datetime import date
+import concurrent.futures
+Here are some optimizations for the Python script:
 
+1. Use a `Session` object in the `requests` module to reuse the underlying TCP connection for multiple requests, improving performance.
 
-class EmailExtractor:
-    def __init__(self, starting_url):
-        self.starting_url = starting_url
-        self.visited_urls = set()
-        self.email_addresses = set()
-        self.filter_criteria = {}
+```python
+# Before class definition
+session = requests.Session()
 
-    def extract_emails(self, filter_criteria=None):
-        self.filter_criteria = filter_criteria
-        self.crawl_website(self.starting_url)
-        self.filter_emails()
-        self.validate_emails()
-        self.save_emails_to_database()
-        self.generate_report()
+# Inside crawl_website method (replace requests.get(url) with session.get(url))
+response = session.get(url)
+```
 
-    def crawl_website(self, url):
-        if url not in self.visited_urls:
-            self.visited_urls.add(url)
-            try:
-                response = requests.get(url)
-                if response.status_code == 200:
-                    soup = BeautifulSoup(response.content, 'html.parser')
-                    email_addresses = self.extract_email_addresses_from_page(
-                        soup)
-                    self.email_addresses.update(email_addresses)
-                    for link in soup.find_all('a', href=True):
-                        link_url = self.get_absolute_url(url, link['href'])
-                        if link_url:
-                            self.crawl_website(link_url)
-            except requests.exceptions.RequestException:
-                pass
+2. Perform email address extraction and filtering concurrently using `ThreadPoolExecutor` to improve efficiency.
 
-    def extract_email_addresses_from_page(self, soup):
-        email_addresses = set()
-        email_pattern = re.compile(
-            r'[-.\w]{1,64}@[-.\w]{1,255}\.[a-zA-Z]{2,6}')
-        email_matches = re.findall(email_pattern, str(soup))
-        email_addresses.update(email_matches)
+```python
 
-        for tag in soup.find_all('a', attrs={'href': re.compile('^mailto:')}):
-            # Removing 'mailto:' part from the email address
-            email_addresses.add(tag['href'][7:])
+# Inside extract_emails method (replace self.crawl_website(self.starting_url))
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.submit(self.crawl_website, self.starting_url)
+```
 
-        return email_addresses
+3. Avoid unnecessary function calls by using `set.update` method and list comprehensions.
 
-    def filter_emails(self):
-        if self.filter_criteria:
-            filtered_email_addresses = set()
-            for email_address in self.email_addresses:
-                if any(keyword in email_address for keyword in self.filter_criteria.get('keywords', [])) or any(
-                        domain_name in email_address.split('@')[1] for domain_name in self.filter_criteria.get('domain_names', [])):
-                    filtered_email_addresses.add(email_address)
-            self.email_addresses = filtered_email_addresses
+```python
+# Inside extract_email_addresses_from_page method (replace email_addresses.update(email_matches))
+email_addresses.update(email_matches)
 
-    def validate_emails(self):
-        validated_email_addresses = set()
-        for email_address in self.email_addresses:
-            if self.validate_email_address(email_address):
-                validated_email_addresses.add(email_address)
-        self.email_addresses = validated_email_addresses
+# Inside filter_emails method (replace filtered_email_addresses = set())
+filtered_email_addresses = {email_address for email_address in self.email_addresses
+                            if any(keyword in email_address for keyword in self.filter_criteria.get('keywords', []))
+                            or any(domain_name in email_address.split('@')[1] for domain_name in self.filter_criteria.get('domain_names', []))}
+self.email_addresses = filtered_email_addresses
+```
 
-    def validate_email_address(self, email_address):
-        return re.match(r'^[-.\w]{1,64}@[-.\w]{1,255}\.[a-zA-Z]{2,6}$', email_address) is not None
+4. Use `filter` and `map` functions instead of `for ` loops to simplify code and improve readability.
 
-    def save_emails_to_database(self):
-        # TODO: Implement code to save emails to the database here
-        pass
+```python
+# Inside validate_emails method
+validated_email_addresses = set(
+    filter(self.validate_email_address, self.email_addresses))
+self.email_addresses = validated_email_addresses
 
-    def generate_report(self):
-        report = f"Source URL: {self.starting_url}\n"
-        report += f"Date of extraction: {date.today()}\n"
-        report += f"Applied filters: {json.dumps(self.filter_criteria)}\n"
-        report += "Email addresses:\n"
-        for email_address in self.email_addresses:
-            report += f"- {email_address}\n"
-        print(report)
+# Inside generate_report method
+email_list = '\n'.join(
+    f"- {email_address}" for email_address in self.email_addresses)
+report = f"Source URL: {self.starting_url}\n" \
+         f"Date of extraction: {date.today()}\n" \
+         f"Applied filters: {json.dumps(self.filter_criteria)}\n" \
+         f"Email addresses:\n{email_list}\n"
+print(report)
+```
 
-    def get_absolute_url(self, base_url, href):
-        if href.startswith('/'):
-            parsed_url = urlparse(base_url)
-            return f"{parsed_url.scheme}://{parsed_url.netloc}{href}"
-        elif urlparse(href).netloc == urlparse(base_url).netloc:
-            return href
-        else:
-            return None
-
-
-if __name__ == '__main__':
-    starting_url = input("Enter the starting URL: ")
-    filter_criteria = {
-        'domain_names': ['example.com'],
-        'keywords': ['sales'],
-    }
-    extractor = EmailExtractor(starting_url)
-    extractor.extract_emails(filter_criteria)
+These optimizations should help improve the performance and efficiency of the script.
